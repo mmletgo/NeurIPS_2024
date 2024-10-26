fb49a1407961bc17938396d4c02702c4bc71d889
mmletgo <zhengrongh@126.com>
2024-10-26 15:36:32 +0800

    fix bug

--- 3dtransformer.py
+++ 3dtransformer.py
@@ -74,24 +74,26 @@ class MultiDimensionalTransformer(nn.Module):
         batch_size, num_wavelengths, seq_len, space_dim = x.size()
 
         # 1. 对时间维度升维
-        x = x.view(
+        x = x.reshape(
             -1, seq_len,
             space_dim)  # (batch_size * num_wavelengths, seq_len, space_dim)
         x = self.expand_time(
             x)  # (batch_size * num_wavelengths, new_seq_len, space_dim)
 
         # 2. 对波长维度升维
-        x = x.view(batch_size, num_wavelengths, -1, space_dim
-                   )  # (batch_size , num_wavelengths, new_seq_len, space_dim)
+        x = x.reshape(
+            batch_size, num_wavelengths, -1, space_dim
+        )  # (batch_size , num_wavelengths, new_seq_len, space_dim)
         batch_size, num_wavelengths, new_seq_len, space_dim = x.size()
         x = x.permute(
             0, 2, 1,
             3)  # (batch_size, new_seq_len, num_wavelengths, space_dim)
-        x = x.view(-1, num_wavelengths, space_dim
-                   )  # (batch_size * new_seq_len, num_wavelengths, space_dim)
+        x = x.reshape(
+            -1, num_wavelengths, space_dim
+        )  # (batch_size * new_seq_len, num_wavelengths, space_dim)
         x = self.expand_wavelength(
             x)  # (batch_size * new_seq_len, new_num_wavelengths, space_dim)
-        x = x.view(
+        x = x.reshape(
             batch_size, new_seq_len, -1, space_dim
         )  # (batch_size, new_seq_len, new_num_wavelengths, space_dim)
         batch_size, new_seq_len, new_num_wavelengths, space_dim = x.size()
@@ -100,67 +102,67 @@ class MultiDimensionalTransformer(nn.Module):
         x = x.permute(
             0, 2, 1,
             3)  # (batch_size, new_num_wavelengths, new_seq_len, space_dim)
-        x = x.view(
+        x = x.reshape(
             -1, new_seq_len, space_dim
         )  # (batch_size * new_num_wavelengths, new_seq_len, space_dim)
         x = self.time_space_transformer(x)  # 时间-空间
 
-        x = x.view(
+        x = x.reshape(
             batch_size, new_num_wavelengths, new_seq_len, space_dim
         )  # (batch_size, new_num_wavelengths, new_seq_len, space_dim)
         x = x.permute(
             0, 3, 2,
             1)  # (batch_size, space_dim, new_seq_len, new_num_wavelengths)
-        x = x.view(
+        x = x.reshape(
             -1, new_seq_len, new_num_wavelengths
         )  # (batch_size * space_dim, new_seq_len, new_num_wavelengths)
         x = self.time_wavelength_transformer(x)  # 时间-波长
 
-        x = x.view(
+        x = x.reshape(
             batch_size, space_dim, new_seq_len, new_num_wavelengths
         )  # (batch_size, space_dim, new_seq_len, new_num_wavelengths)
         x = x.permute(
             0, 3, 1,
             2)  # (batch_size, new_num_wavelengths, space_dim, new_seq_len)
-        x = x.view(
+        x = x.reshape(
             -1, space_dim, new_seq_len
         )  # (batch_size * new_num_wavelengths, space_dim, new_seq_len)
         x = self.space_time_transformer(x)  # 空间-时间
 
-        x = x.view(
+        x = x.reshape(
             batch_size, new_num_wavelengths, space_dim, new_seq_len
         )  # (batch_size, new_num_wavelengths, space_dim, new_seq_len)
         x = x.permute(
             0, 3, 2,
             1)  # (batch_size, new_seq_len, space_dim, new_num_wavelengths)
-        x = x.view(-1, space_dim, new_num_wavelengths)
+        x = x.reshape(-1, space_dim, new_num_wavelengths)
         x = self.space_wavelength_transformer(x)  # 空间-波长
 
-        x = x.view(
+        x = x.reshape(
             batch_size, new_seq_len, space_dim, new_num_wavelengths
         )  # (batch_size, new_seq_len, space_dim, new_num_wavelengths)
         x = x.permute(
             0, 2, 3,
             1)  # (batch_size, space_dim, new_num_wavelengths, new_seq_len)
-        x = x.view(
+        x = x.reshape(
             -1, new_num_wavelengths, new_seq_len
         )  # (batch_size * space_dim, new_num_wavelengths, new_seq_len)
         x = self.wavelength_time_transformer(x)  # 波长-时间
 
-        x = x.view(
+        x = x.reshape(
             batch_size, space_dim, new_num_wavelengths, new_seq_len
         )  # (batch_size, space_dim, new_num_wavelengths, new_seq_len)
         x = x.permute(
             0, 3, 2,
             1)  # (batch_size, new_seq_len, new_num_wavelengths, space_dim)
-        x = x.view(
+        x = x.reshape(
             -1, new_num_wavelengths, space_dim
         )  # (batch_size*new_seq_len, new_num_wavelengths, space_dim)
         x = self.wavelength_space_transformer(x)  # 波长-空间
 
-        x = x.view(batch_size, new_seq_len, new_num_wavelengths, space_dim)
+        x = x.reshape(batch_size, new_seq_len, new_num_wavelengths, space_dim)
         # 4. 最终 MLP 输出层
-        x = x.view(batch_size, -1)  # 展平为 (batch_size, feature_dim)
+        x = x.reshape(batch_size, -1)  # 展平为 (batch_size, feature_dim)
         output = self.final_mlp(x)  # 输出 (batch_size, 283)
 
         return output
